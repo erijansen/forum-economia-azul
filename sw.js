de3b65c00a91caf7cc9414933d9a5a1d844505a7
erijansen <maciel.erijansen@gmail.com>
2026-09-02 20:54:56 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "forum-v9-2026-09-02";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./app-icon.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    .then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(fetch(e.request).then(r => {
+    const copy = r.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy));
+    return r;
+  }).catch(() => caches.match(e.request).then(r => r || caches.match("./index.html"))));
+});
